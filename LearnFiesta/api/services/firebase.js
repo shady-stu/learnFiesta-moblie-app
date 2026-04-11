12e45da068e890e1e41a455c6d32f6334af9578e
SHADY SAWALHA <s12323788@stu.najah.edu>
2026-04-11 23:10:25 +0300

    D Tsx

--- LearnFiesta/api/services/firebase.js
+++ LearnFiesta/api/services/firebase.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth"; // ✅ add this
+import { getAuth } from "firebase/auth"; 
 
 const firebaseConfig = {
   apiKey: "AIzaSyBCPY0xTaWhhPiFjo4DErNORiAwdhg1clE",
@@ -13,6 +13,6 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 
-// ✅ export BOTH
+
 export const db = getFirestore(app);
 export const auth = getAuth(app);
