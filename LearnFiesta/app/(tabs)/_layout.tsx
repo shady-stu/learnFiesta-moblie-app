30acece3114119eb9a96aef4fd717ff03e47d619
SHADY SAWALHA <s12323788@stu.najah.edu>
2026-04-13 18:40:02 +0300

    add

--- LearnFiesta/app/(tabs)/_layout.tsx
+++ LearnFiesta/app/(tabs)/_layout.tsx
@@ -57,7 +57,7 @@ export default function TabsLayout() {
       />
 
       <Tabs.Screen
-        name="courses"
+        name="MyCourses"
         options={{
           title: 'My Courses',
           tabBarIcon: ({ color, focused }) => (
