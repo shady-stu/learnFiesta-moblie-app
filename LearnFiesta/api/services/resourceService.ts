a3566ac7dfa8c04a36d65e7d25562aee00c8ea6a
Qusai-Nazzal <s12324944@stu.najah.edu>
2026-05-09 02:45:32 +0300

    details done2

--- LearnFiesta/api/services/resourceService.ts
+++ LearnFiesta/api/services/resourceService.ts
@@ -4,21 +4,17 @@ import { Resource } from "@/types/lesson";
 
 export const fetchResources = async (courseId: string): Promise<Resource[]> => {
 
-    const q = query(collection(db, "resources"), where("courseId", "==", courseId));
-    const snapshot = await getDocs(q);
+     const q = query(collection(db, "resources"), where("courseId", "==", courseId));
+     const snapshot = await getDocs(q);
 
-    return snapshot.docs.map((doc) => {
-        const data = doc.data();
-        return {
-            id: doc.id,
-
-            courseId: data.courseId,
-
-            title: data.title,
-
-            type: data.type,
-
-            url: data.url,
+     return snapshot.docs.map((doc) => {
+         const data = doc.data();
+         return {
+             id: doc.id,
+             courseId: data.courseId,
+             title: data.title,
+             type: data.type,
+             url: data.url,
         };
     });
 };
