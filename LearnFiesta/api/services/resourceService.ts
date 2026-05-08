cfc8dd3d1759cab65d93512d3340741a660fdb52
Qusai-Nazzal <s12324944@stu.najah.edu>
2026-05-09 02:54:06 +0300

    details done2

--- LearnFiesta/api/services/resourceService.ts
+++ LearnFiesta/api/services/resourceService.ts
@@ -2,12 +2,9 @@ import {collection, getDocs, query, where} from "firebase/firestore";
 import {Resource} from "@/types/lesson";
 import {db} from "@/api/services/firebase";
 
-
 export const fetchResources = async (courseId : string) : Promise<Resource[]> => {
-
      const q = query(collection(db, "resources"), where("courseId", "==", courseId));
      const snapshot = await getDocs(q);
-
      return snapshot.docs.map((doc) => {
          const data = doc.data();
          return {
